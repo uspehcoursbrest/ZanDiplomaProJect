32466f57780a428f9f6e2002a8002fff1efa2c91
ArtemBrest <artoym.zan@gmail.com>
2018-11-16 23:24:51 +0300

    адоптировал

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 window.onload = function () {
     function DataTime() {
         var date = new Date(),
-            day = date.getDate(),
+            day = date.getDay(),
+            dayNames = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday"],
             month = date.getMonth(),
-            monthArr = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"],
+            monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
             year = date.getFullYear(),
             hour = date.getHours(),
             min = date.getMinutes();
 
         if(day < 10) day = "0" + day;
-        if(hour < 10) hour = "0" + day;
+        if(hour < 10) hour = "0" + hour;
         if(min < 10) min = "0" + min;
 
-        document.getElementById("data") .innerHTML = day + " " + monthArr[month] + " " + year;
+        document.getElementById("date") .innerHTML = dayNames + " ," + month + monthArr[month] + " " + year;
         document.getElementById("time") .innerHTML = hour + ":" + min;
 
     }
     var timer;
-    function clockStart() {
+    function DataTimeStart() {
         timer = setInterval(DataTime, 60000);
         DataTime();
 
 
     }
-    clockStart();
+    DataTimeStart();
 }
