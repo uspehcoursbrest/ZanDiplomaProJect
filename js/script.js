4fc0fcf181fefe9932c46c69f7bb816a5373f267
ArtemBrest <artoym.zan@gmail.com>
2018-11-20 20:41:44 +0300

    выполнил дз

--- js/script.js
+++ js/script.js
@@ -10,20 +10,44 @@ window.onload = function () {
             hour = date.getHours(),
             min = date.getMinutes();
 
-        if(day < 10) day = "0" + day;
-        if(hour < 10) hour = "0" + hour;
-        if(min < 10) min = "0" + min;
+        if (day < 10) day = "0" + day;
+        if (hour < 10) hour = "0" + hour;
+        if (min < 10) min = "0" + min;
 
         document.getElementById("date").innerHTML = dayNames[weekDay] + " ," + day + " " + monthArr[month] + " " + year;
         document.getElementById("time").innerHTML = hour + ":" + min;
 
     }
+
     var timer;
+
     function DataTimeStart() {
         timer = setInterval(DataTime, 60000);
         DataTime();
 
 
     }
+
     DataTimeStart();
 }
+$(document).ready(function () {
+
+    var btnUp = $(".up");
+    btnUp.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1000) {
+            btnUp.fadeIn();
+        } else {
+            btnUp.fadeOut();
+
+        }
+
+    })
+    btnUp.click(function () {
+        $("body,html").animate({
+            scrollTop: 0
+        }, 1000);
+        return false;
+    })
+})
+
